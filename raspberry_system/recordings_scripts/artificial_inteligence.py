96b2518e6fe944e153d1389c51baccf093550f48
rafaelcoder18 <rafaelabcferreira@gmail.com>
2024-03-25 17:46:16 -0300

    inclusão do dataset

--- raspberry_system/recordings_scripts/artificial_inteligence.py
+++ raspberry_system/recordings_scripts/artificial_inteligence.py
@@ -9,8 +9,16 @@ Descrição: Este script é responsável por aplicar o modelo de IA nos arquivos
 from ultralytics import YOLO # Biblioteca de IA
 import os, shutil # Bibliotecas do sistema
 
+<<<<<<< Updated upstream:raspberry_system/recordings_scripts/artificial_inteligence.py
 model = YOLO('/home/rafael/patromonial-system/raspberry_system/recordings_scripts/train7/weights/best.pt') # Carrega o modelo de IA
 cont = 0 # Define um contador para os arquivos de vídeo
+=======
+model = YOLO('C:/Users/rafae/OneDrive/Área de Trabalho/patromonial-system/runs/detect/train7/weights/best.pt') # Carrega o modelo de IA
+cont = 0 # Define um contador para os arquivos de vídeo
+
+results = model.predict('https://www.youtube.com/watch?v=CNOqheVvlJw', show=True)
+'''
+>>>>>>> Stashed changes:raspberry_system/artificial_inteligence.py
 # Aplica o modelo de IA nos arquivos de vídeo da pasta de análise e move para a pasta de gravação.
 
 while True:
@@ -19,3 +27,4 @@ while True:
           print(results)
           shutil.move(f'/home/rafael/patromonial-system/raspberry_system/analisys/{videos}', f'/home/rafael/patromonial-system/raspberry_system/recordings/recording_{cont}.avi')
           cont += 1
+'''
--- teste.py
+++
@@ -1,6 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("runs/detect/train17/weights/best.pt")
-# accepts all formats - image/dir/Path/URL/video/PIL/ndarray. 0 for webcam
-results = model.predict(source="https://www.youtube.com/watch?v=BtMmymOxdjc", show=True) # Display preds. Accepts all YOLO predict arguments
-
